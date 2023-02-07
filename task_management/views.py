3566ad96430fda48007024f6d2329c6e2eb38443
Wenderson-Oscar <wendersonoscardev@gmail.com>
2023-02-07 18:01:58 -0300

    CRUD of tasks

--- task_management/views.py
+++ task_management/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tarefa
 from .forms import TaskForms
 
 # Create your views here.
 def list_task(request):
     task = Tarefa.objects.all()
-    return render(request, 'home.html', {'task': task})
+    return render(request, 'task/list_task.html', {'task': task})
 
 def add_task(request):
     if request.method == "POST":
@@ -13,8 +13,28 @@ def add_task(request):
         if form.is_valid():
             task = form.save(commit=False)
             form.save()
-            return redirect('home')
+            return redirect('detail_task', id=task.id)
     else:
         form = TaskForms()
     return render(request, 'task/add_task.html', {'form': form})
 
+def detail_task(request, id):
+    task = get_object_or_404(Tarefa, pk=id)
+    return render(request, 'task/detail_task.html', {'task': task})
+
+def edit_task(request, id):
+    task = get_object_or_404(Tarefa, pk=id)
+    if request.method == "POST":
+        form = TaskForms(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save(commit=False)
+            form.save()
+            return redirect('detail_task', id=task.id)
+    else:
+        form = TaskForms(instance=task)
+    return render(request, 'task/edit_task.html', {'form': form})
+
+def delete_task(request, id):
+    task = get_object_or_404(Tarefa, pk=id)
+    task.delete()
+    return redirect('list_task')
