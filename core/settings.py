035f2be46e842c52f6e3f1e66caace259625f9ef
Wenderson-Oscar <wendersonoscardev@gmail.com>
2023-02-13 18:45:38 -0300

    melhorando front-end

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 else:
